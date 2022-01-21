624efe13d9f836ff9a07ad7ff1c9af0b8e290a03
MrRazorez <viantosa1999@gmail.com>
2022-01-21 20:37:49 +0700

    tambah fitur

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BUMDES | Badan Usaha Milik Desa</title>
+    <link rel="stylesheet" href="/fix/style.css">
+    <link rel="stylesheet" href="/fix/semantic/custom-grid.css">
+    <link rel="stylesheet" href="/fix/semantic/custom-image.css">
+    <link rel="stylesheet" href="/fix/semantic/custom-header.css">
+</head>
+<body>
+    @include('komponen.header-home')
+    @include('komponen.konten-home')
+</body>
+</html>
---
+++ resources/views/komponen/konten-home.blade.php
@@ -0,0 +1,22 @@
+<div class="konten">
+    <div class="ui equal width centered grid">
+        <div class="three wide column">
+            <a href="#" style="text-decoration: none;"class="ui small fluid image">
+                <code style="color: black">Warung BUMDES</code>
+                <img src="/fix/warung.png" alt="Warung BUMDES">
+            </a>
+        </div>
+        <div class="three wide column">
+            <a href="#" style="text-decoration: none;"class="ui small fluid image">
+                <code style="color: black">Fotokopi</code>
+                <img src="/fix/fotokopi.png" alt="Fotokopi">
+            </a>
+        </div>
+        <div class="three wide column">
+            <a href="#" style="text-decoration: none;"class="ui small fluid image">
+                <code style="color: black">Kolam Ikan</code>
+                <img src="/fix/ikan.png" alt="Kolam Ikan">
+            </a>
+        </div>
+    </div>
+</div>
